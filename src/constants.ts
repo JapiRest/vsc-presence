78b65b1784541e202792802fca6a3c781e954af7
DanPlayz0 <damuffin36@gmail.com>
2021-08-22 19:48:06 -0700

    Raw Image

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const baseUrl = 'https://japi.rest/presence/v1/';
 export const keyRegex = /JAPI\..{50,54}/;
 export const fileSizes = [' bytes', 'kb', 'mb', 'gb', 'tb'];
-export const extensionImageURL = 'https://github.com/vscode-icons/vscode-icons/blob/master/icons/';
+export const extensionImageURL = 'https://raw.githubusercontent.com/vscode-icons/vscode-icons/blob/master/icons/';
